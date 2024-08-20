2112be8e45b9ba66a0540f8557d00d94270ab888
Christoph Kappel <christoph@unexist.dev>
2024-08-20 15:44:41 +0200

    Added subtle nix

---
+++ subtle.nix
@@ -0,0 +1,16 @@
+{ stdenv, fetchhg }:
+
+stdenv.mkDerivation {
+  pname = "subtle";
+  version = "0.9999";
+  src = fetchhg {
+    url = "https://hg.unexist.dev/subtle
+    rev = "7881370e398a";
+    hash = "7881370e398a";
+  };
+
+  installFlags = [ "PREFIX=$(out)" ];
+
+  system = builtins.currentSystem;
+  buildInputs = [ruby];
+}
