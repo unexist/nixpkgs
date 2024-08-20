6e872b33c8d1a4762cda4d50ef0bb0557acf6520
Christoph Kappel <christoph@unexist.dev>
2024-08-20 15:48:45 +0200

    Fixes missing quoe and semicolon

--- subtle.nix
+++ subtle.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
   pname = "subtle";
   version = "0.9999";
   src = fetchhg {
-    url = "https://hg.unexist.dev/subtle
+    url = "https://hg.unexist.dev/subtle";
     rev = "7881370e398a";
     hash = "7881370e398a";
   };
