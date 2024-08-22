1e0c72fa50fea6fc5abe0964186e13edba6dcc5d
Christoph Kappel <christoph@unexist.dev>
2024-08-22 14:42:14 +0200

    Updated package

--- subtle.nix
+++ subtle.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchhg }:
+{ lib, stdenv, fetchhg, libX11, libXinerama, libXft, libXrandr, ruby, .. }:
 
 stdenv.mkDerivation {
   pname = "subtle";
@@ -6,11 +6,11 @@ stdenv.mkDerivation {
   src = fetchhg {
     url = "https://hg.unexist.dev/subtle";
     rev = "7881370e398a";
-    hash = "7881370e398a";
+    hash = "cb2c4a304ff96e56b45416de44bf338e39cfacc9ec17a712a294334024f2ceb9f7fa25ab2aea336e2b3d2060ff989c6e23700a5ba123dbd14694a60099297174";
   };
 
   installFlags = [ "PREFIX=$(out)" ];
 
   system = builtins.currentSystem;
-  buildInputs = [ruby];
+  buildInputs = [ libX11 libXinerama libXft ruby ];
 }
